03d9af66d1a8dca27ab18e5c2822bb6f6daa2d71
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-12-21 10:21:45 +0530

    Created profile page UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   return (
     <div className='bg-slate-200'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+      <div className='flex items-center justify-between max-w-6xl p-3 mx-auto'>
         <Link to='/'>
-          <h1 className='font-bold'>Auth App</h1>
+          <h1 className='font-bold'>Boffo Prject</h1>
         </Link>
         <ul className='flex gap-4'>
           <Link to='/'>
@@ -18,7 +18,7 @@ export default function Header() {
           </Link>
           <Link to='/profile'>
             {currentUser ? (
-              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+              <img src={currentUser.profilePicture} alt='profile' className='object-cover rounded-full h-7 w-7' />
             ) : (
               <li>Sign In</li>
             )}
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+
+  const {currentUser} = useSelector(state => state.user)
+
   return (
-    <>
-      
-    </>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src= {currentUser.profilePicture} alt="profile" className='self-center object-cover w-32 h-32 mt-2 rounded-full cursor-pointer'/>
+
+        <input 
+          defaultValue={currentUser.username} 
+          type="text" 
+          id="username" 
+          placeholder='Username' 
+          className='p-3 font-semibold rounded-lg bg-slate-100' 
+        />
+        <input 
+          defaultValue={currentUser.email} 
+          type="email" 
+          id="email" 
+          placeholder='Email' 
+          className='p-3 font-semibold rounded-lg bg-slate-100' 
+        />
+        <input 
+          type="password" 
+          id="password" 
+          placeholder='Password' 
+          className='p-3 font-semibold rounded-lg bg-slate-100' 
+        />
+
+        <button className='p-2 font-semibold text-white uppercase rounded-lg bg-slate-700 hover:opacity-90 hover:font-bold disabled:opacity-75'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='font-medium text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='font-medium text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
